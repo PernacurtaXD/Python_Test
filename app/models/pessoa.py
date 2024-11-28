cf8deb98a329f3a0b93d045fb19469bc70bee9f5
PernacurtaXD <168147492+PernacurtaXD@users.noreply.github.com>
2024-11-28 17:55:14 +0000

    models / test_pessoa

--- app/models/pessoa.py
+++ app/models/pessoa.py
@@ -4,10 +4,27 @@ from app.models.enum.sexo import Sexo
 class Pessoa:
     def __init__(self, nome: str, idade: str, sexo: Sexo):
         self.nome = self._nome_test(nome)
-        self.idade = idade
+        self.idade = self._idade_test(idade)
         self.sexo = sexo
 
     def _nome_test(self, nome):
         if not nome.strip():
+            raise TypeError("O nome digitado não pode ser vazio.")
+        
+        if nome != "Luis":
             raise TypeError("O nome digitado está incorreto.")
+        
         return nome
+    
+    def _idade_test(self, idade):
+        
+        if not idade.strip():
+            raise TypeError("Idade digitada incorreta.")
+        
+
+        if idade != "19":
+            raise TypeError("Idade inválida")
+            
+        return idade  
+
+
--- app/test/test_pessoa.py
+++ app/test/test_pessoa.py
@@ -3,8 +3,18 @@ from app.models.pessoa import Pessoa
 from ..models.enum.sexo import Sexo
 
 
+def test_nome_vazio():
+    with pytest.raises(TypeError, match= "O nome digitado não pode ser vazio."):
+        Pessoa("", "19", Sexo.MASCULINO.value)
 
+def test_idade_vazio():
+    with pytest.raises(TypeError, match= "Idade digitada incorreta."):
+        Pessoa("Luis", "", Sexo.MASCULINO.value)
 
 def test_nome_invalido():
     with pytest.raises(TypeError, match= "O nome digitado está incorreto."):
-        Pessoa("", "19", Sexo.MASCULINO.value)
+        Pessoa("Marcos", "19", Sexo.MASCULINO.value)
+
+def test_idade_invalida():
+    with pytest.raises(TypeError, match= "Idade inválida"):
+        Pessoa("Luis", "15", Sexo.MASCULINO.value)
